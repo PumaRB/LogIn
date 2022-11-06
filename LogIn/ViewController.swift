3c64a0f319350d48659d5a42f56eec2c127c8e58
Puma <Pumaredblue@gmail.com>
2022-11-06 21:38:01 +0200

    UI and alerts

--- LogIn/ViewController.swift
+++ LogIn/ViewController.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func forgotPasswordButton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "000000", message: "Is it your password", preferredStyle: UIAlertController.Style.alert)
+    
+        alert.addAction(UIAlertAction(title: "Got it", style: UIAlertAction.Style.default, handler: nil))
+
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func forgotNameButton(_ sender: UIButton) {
+    
+        let alert = UIAlertController(title: "User", message: "Is it your uername", preferredStyle: UIAlertController.Style.alert)
+    
+        alert.addAction(UIAlertAction(title: "Got it", style: UIAlertAction.Style.default, handler: nil))
+
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
